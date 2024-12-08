1280b4ef53c04c0848527e2b47e86297445c8b4e
Shiiyu <40838999+Shiiyu@users.noreply.github.com>
2024-12-08 17:58:24 -0500

    feat: day 1

---
+++ js/src/day01.mjs
@@ -0,0 +1,48 @@
+import {readFileSync} from "fs";
+
+const INPUT = readFileSync("../../inputs/day01.txt", "utf8");
+
+export function solve_a() {
+  const [listOne, listTwo] = INPUT.split("\n")
+    .map(l => l.split(/\s+/)
+      .map(n => Number(n)))
+    .flat()
+    .reduce((nums, num) => {
+      nums[nums[0].length > nums[1].length ? 1 : 0].push(num);
+
+      return nums;
+    }, [[], []])
+    .map(nums => nums.sort());
+  let sum = 0;
+
+  for(let i = 0; i < listOne.length; i++)
+    sum += Math.abs(listOne[i] - listTwo[i]);
+
+  return sum;
+}
+export function solve_b() {
+  const [listOne, listTwo] = INPUT.split("\n")
+    .map(l => l.split(/\s+/)
+      .map(n => Number(n)))
+    .flat()
+    .reduce((nums, num) => {
+      nums[nums[0].length > nums[1].length ? 1 : 0].push(num);
+
+      return nums;
+    }, [[], []]);
+  let sum = 0;
+
+  for(let i = 0; i < listOne.length; i++) {
+    let count = 0;
+
+    for(let j = 0; j < listTwo.length; j++) {
+      if(listOne[i] === listTwo[j])
+        count++;
+    }
+
+    sum += listOne[i] * count;
+    count = 0;
+  }
+
+  return sum;
+}
